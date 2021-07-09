ba6a1b7ad0626dad72c875ce619373d272c14db4
Diego Oliveira <86024091+OliverDiego10@users.noreply.github.com>
2021-07-09 11:18:09 -0300

    javaScript-parte1

---
+++ tipos-dados.js
@@ -0,0 +1,26 @@
+const inteiro = 10;
+const decimal = 10.5;
+const bool = false;
+const nome = "Diego";
+let copo = null;
+let televisao = undefined;
+let pessoa = {
+    nome: "Diego"
+}
+const symb = Symbol()
+
+
+
+console.log("tipo de variavel inteiro", typeof inteiro);
+console.log("tipo de variavel decimal", typeof decimal);
+console.log("tipo de variavel bool", typeof bool);
+console.log("tipo de variavel nome", typeof nome);
+console.log("tipo de variavel copo", typeof copo); // metodo null
+console.log("tipo de variavel televisao", typeof televisao); // metodo undefined por nao definir o valor
+console.log("tipo de variavel pessoa", typeof pessoa); //a variavel pessoa é objeto
+console.log("tipo de variavel Symbol", typeof Symbol);
+
+
+
+
+
---
+++ variaveis-constantes.js
@@ -0,0 +1,8 @@
+var cor = "branca";  //pouco usado por coausar problemas de bugs
+
+let nome = "Diego";  // os mais usados
+const empresa = "Devaria";// os mais usados
+
+
+
+console.log(cor, nome, empresa);
